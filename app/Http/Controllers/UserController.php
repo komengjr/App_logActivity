ea57aa734c48c4a96812169d78bfdb5ab3102903
Agus Raharjo <agusprasetyoraharjo@gmail.com>
2024-04-05 05:02:17 +0700

    updateapires6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -429,7 +429,7 @@ class UserController extends Controller
     {
         $url = "http://inventory.pramita.co.id:8000/api/datainventaris/pa";
 
-        $response = @file_get_contents($url);
+        $response = @file_get_contents($url ,false, $context);
         $newsData = json_decode($response);
         dd($response);
         return view('userleader.customtask.lengkapi',['data'=>$newsData]);
