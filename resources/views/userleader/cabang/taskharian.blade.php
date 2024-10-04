1931e0ad9d83a30ee8df65d49c48369e7a18a3ec
Agus Raharjo <agusprasetyoraharjo@gmail.com>
2024-10-04 11:10:09 +0700

    updatelogbaru

--- resources/views/userleader/cabang/taskharian.blade.php
+++ resources/views/userleader/cabang/taskharian.blade.php
@@ -1,5 +1,5 @@
 <style>
-    #acording-button:hover{
+    #acording-button:hover {
         background: rgb(147, 162, 162);
         cursor: pointer;
     }
@@ -18,15 +18,18 @@
                 <div class="col-lg-12">
                     <div id="accordion1">
                         <div class="card mb-2">
-                            <div class="card-header" data-toggle="collapse" data-target="#collapse-1" id="acording-button">
-                                <h5><span class="badge badge-primary">1</span> <span class="badge badge-dark">CHECKLIST LAPORAN KRITIS</span></h5>
+                            <div class="card-header" data-toggle="collapse" data-target="#collapse-1"
+                                id="acording-button">
+                                <h5><span class="badge badge-primary">1</span> <span class="badge badge-dark">CHECKLIST
+                                        LAPORAN KRITIS</span></h5>
                             </div>
 
                             <div id="collapse-1" class="collapse" data-parent="#accordion1">
                                 <form action="{{ url('user/user/handledatacabang/postrecorddata', []) }}"
                                     method="post">
                                     @csrf
-                                    <input type="text" name="kd_cabang" value="{{ $cabang->kd_cabang }}" id="" hidden>
+                                    <input type="text" name="kd_cabang" value="{{ $cabang->kd_cabang }}"
+                                        id="" hidden>
                                     <div class="card-body">
                                         @foreach ($data as $item)
                                             <li
@@ -70,34 +73,43 @@
                             </div>
                         </div>
                         <div class="card mb-1">
-                            <div class="card-header " data-toggle="collapse"  data-target="#collapse-2" id="acording-button">
-                                <h5><span class="badge badge-primary">2</span> <span class="badge badge-dark">CHECKLIST SISTEM SERVER & BACKUP DATA BISONE HARIAN</span></h5>
+                            <div class="card-header " data-toggle="collapse" data-target="#collapse-2"
+                                id="acording-button">
+                                <h5><span class="badge badge-primary">2</span> <span class="badge badge-dark">CHECKLIST
+                                        SISTEM SERVER & BACKUP DATA BISONE HARIAN</span></h5>
                             </div>
                             <div id="collapse-2" class="collapse" data-parent="#accordion1">
                                 @php
-                                    $cekbackup = DB::table('users_backup_harian')->where('kd_cabang',$cabang->kd_cabang)->where('tgl_backup_harian',date('Y-m-d'))->first();
+                                    $cekbackup = DB::table('users_backup_harian')
+                                        ->where('kd_cabang', $cabang->kd_cabang)
+                                        ->where('tgl_backup_harian', date('Y-m-d'))
+                                        ->first();
                                 @endphp
                                 @if ($cekbackup)
                                     <div class="card-body">
                                         <i class="fa fa-check-circle"></i>
                                     </div>
                                 @else
-                                    <form action="{{ url('user/user/handledatacabang/postrecorddatabackup', []) }}" method="post">
+                                    <form action="{{ url('user/user/handledatacabang/postrecorddatabackup', []) }}"
+                                        method="post">
                                         @csrf
                                         <div class="card-body pb-0 mb-0">
                                             <div class="row">
                                                 <div class="col-md-6">
                                                     <label for="">SISTEM</label>
-                                                    <select name="sistem_backup" class="form-control" id="" required>
+                                                    <select name="sistem_backup" class="form-control" id=""
+                                                        required>
                                                         <option value="">Pilih Status</option>
                                                         <option value="OK">OK</option>
                                                         <option value="NOT OK">NOT OK</option>
                                                     </select>
-                                                    <input type="text" name="kd_cabang" id="" value="{{ $cabang->kd_cabang }}" hidden>
+                                                    <input type="text" name="kd_cabang" id=""
+                                                        value="{{ $cabang->kd_cabang }}" hidden>
                                                 </div>
                                                 <div class="col-md-6">
                                                     <label for="">PROSES BACK UP</label>
-                                                    <select name="proses_backup" class="form-control" id="" required>
+                                                    <select name="proses_backup" class="form-control" id=""
+                                                        required>
                                                         <option value="">Pilih Status</option>
                                                         <option value="OK">OK</option>
                                                         <option value="NOT OK">NOT OK</option>
@@ -105,7 +117,36 @@
                                                 </div>
                                                 <div class="col-md-12">
                                                     <label for="">Deskripsi</label>
-                                                    <textarea name="deskripsi_backup" class="form-control" id="summernoteEditor" cols="5" rows="10" required></textarea>
+                                                    @php
+                                                        $data = DB::connection('second_db')
+                                                            ->table('log')
+                                                            ->where('logBranchCode', $cabang->kd_cabang)
+                                                            ->Where('logDate','like', '%' . date('Y-m-d') . '%',)
+                                                            ->first();
+
+                                                    @endphp
+                                                    @if ($data)
+                                                    <textarea name="deskripsi_backup" class="form-control" id="summernoteEditor" cols="5" rows="10" required>
+                                                        @php
+                                                            $data = str_replace('UP', '<br>UP', $data->logMessage);
+                                                            $data1 = str_replace('Service :', '<br>Service :', $data);
+                                                            $data2 = str_replace('Pacs', '<br>Pacs', $data1);
+                                                            $data3 = str_replace('Web Server', '<br>Web Server', $data2);
+                                                            $data4 = str_replace('Report Server', '<br>Report Server', $data3);
+                                                            $data5 = str_replace('MariaDB Server', '<br>MariaDB Server', $data4);
+                                                            $data6 = str_replace('----------', '<br>----------', $data5);
+                                                            $data7 = str_replace('Resource :', '<br>Resource :', $data6);
+                                                            $data8 = str_replace('folder', '<br>folder', $data7);
+                                                            $data9 = str_replace('Connectivity:', '<br>Connectivity:<br>', $data8);
+                                                            echo $data9;
+                                                        @endphp
+                                                    </textarea>
+                                                    @else
+                                                    <textarea name="deskripsi_backup" class="form-control" id="summernoteEditor" cols="5" rows="10" required>
+
+                                                    </textarea>
+                                                    @endif
+
                                                 </div>
                                             </div>
                                         </div>
