a2b0422d1eec303446ed46aa568a4bc64b2b43e0
Agus Raharjo <agusprasetyoraharjo@gmail.com>
2024-10-04 14:03:08 +0700

    updatelogbaru2

--- resources/views/userleader/masterdata/detail-laporan.blade.php
+++ resources/views/userleader/masterdata/detail-laporan.blade.php
@@ -1,8 +1,16 @@
+<style>
+    img {
+        max-width: 100%;
+        height: auto;
+        width: auto\9;
+        /* ie8 */
+    }
+</style>
 <div class="card">
     {{-- <img src="https://via.placeholder.com/800x500" class="card-img-top" alt="Card image cap"> --}}
     <div class="card-body bg-dark">
-        <h5 class="card-title" style="color: white;">Report<button style="float: right;"
-                class="btn-danger" data-dismiss="modal"><i class="fa fa-close"></i></button></h5>
+        <h5 class="card-title" style="color: white;">Report<button style="float: right;" class="btn-danger"
+                data-dismiss="modal"><i class="fa fa-close"></i></button></h5>
         {{-- <p class="card-text">{{$item->alamat}}</p> --}}
 
     </div>
@@ -26,18 +34,18 @@
                         <div class="col-sm-4 invoice-col">
                             From
                             <address>
-                                <strong>{{$data->nama_user}}</strong><br>
-                                {{$data->divisi}}<br>
-                                Email: {{$data->email}}
+                                <strong>{{ $data->nama_user }}</strong><br>
+                                {{ $data->divisi }}<br>
+                                Email: {{ $data->email }}
                             </address>
                         </div>
                         <!-- /.col -->
                         <div class="col-sm-4 invoice-col">
                             To
                             <address>
-                                <strong>{{$data->nama_cabang}}</strong><br>
-                                {{$data->phone}}<br>
-                                {{$data->alamat}}
+                                <strong>{{ $data->nama_cabang }}</strong><br>
+                                {{ $data->phone }}<br>
+                                {{ $data->alamat }}
                             </address>
                         </div>
                         <!-- /.col -->
@@ -77,18 +85,16 @@
                         <div class="col-lg-6">
                             <p class="lead">Answer :</p>
                             @php
-                                $cekanswer = DB::table('tbl_laporan_user_log')->where('tiket_laporan',$tiket)->first();
+                                $cekanswer = DB::table('tbl_laporan_user_log')->where('tiket_laporan', $tiket)->first();
                             @endphp
                             @if ($cekanswer)
-
                                 @php
                                     echo $cekanswer->deskripsi_penyelesaian;
                                 @endphp
-
                             @else
-                            <p class="bg-light p-2 mt-3 rounded">
-                                <span class="badge bg-warning">Belum Selesai</span>
-                            </p>
+                                <p class="bg-light p-2 mt-3 rounded">
+                                    <span class="badge bg-warning">Belum Selesai</span>
+                                </p>
                             @endif
 
                         </div>
@@ -106,13 +112,13 @@
                         <div class="col-lg-9">
                             <div class="float-sm-right">
                                 @if ($cekanswer)
-                                <button class="btn btn-success m-1" disabled>
-                                    <i class="fa fa-save"></i> Selesai
-                                </button>
+                                    <button class="btn btn-success m-1" disabled>
+                                        <i class="fa fa-save"></i> Selesai
+                                    </button>
                                 @else
-                                <button class="btn btn-warning m-1" disabled>
-                                    <i class="fa fa-info"></i> Belum Selesai
-                                </button>
+                                    <button class="btn btn-warning m-1" disabled>
+                                        <i class="fa fa-info"></i> Belum Selesai
+                                    </button>
                                 @endif
 
                             </div>
