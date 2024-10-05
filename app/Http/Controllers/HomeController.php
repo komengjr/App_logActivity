7be3be893fefb35990b1e7ba6c2fb0748be081d8
Agus Raharjo <agusprasetyoraharjo@gmail.com>
2024-10-05 10:11:11 +0700

    updatelogbaru4

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -313,13 +313,24 @@ class HomeController extends Controller
     }
     public function update_biodata(Request $request)
     {
-        DB::table('tbl_biodata')->where('id_user',Auth::user()->id_user)->update([
-            'nama_lengkap'=>$request->nama,
-            'tgl_lahir'=>$request->tgl_lahir,
-            'tempat_lahir'=>$request->tempat_lahir,
-            'gambar' => $request->file('gambar')->storeAs('data_file/fileupload/' . auth::user()->email, auth::user()->id_user . '' . 'pp.jpg'),
-            'alamat'=>$request->alamat,
-        ]);
+        if ($request->file == "") {
+            DB::table('tbl_biodata')->where('id_user',Auth::user()->id_user)->update([
+                'nama_lengkap'=>$request->nama,
+                'tgl_lahir'=>$request->tgl_lahir,
+                'tempat_lahir'=>$request->tempat_lahir,
+                'alamat'=>$request->alamat,
+            ]);
+        } else {
+            DB::table('tbl_biodata')->where('id_user',Auth::user()->id_user)->update([
+                'nama_lengkap'=>$request->nama,
+                'tgl_lahir'=>$request->tgl_lahir,
+                'tempat_lahir'=>$request->tempat_lahir,
+                'gambar' => $request->file('gambar')->storeAs('data_file/fileupload/' . auth::user()->email, auth::user()->id_user . '' . 'pp.jpg'),
+                'alamat'=>$request->alamat,
+            ]);
+        }
+
+
         Session::flash('sukses','Berhasil Update');
         return redirect()->back();
     }
