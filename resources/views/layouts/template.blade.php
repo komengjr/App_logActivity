4c2377e738dd3d2916b980090c4f0bc59016112d
komengjr <47206605+komengjr@users.noreply.github.com>
2026-06-30 08:01:08 +0700

    updateselaludaad

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,8 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js"
-        data-client-key="SB-Mid-client-TzBZ-AB8Hng2jGB_"></script>
     <link rel="icon" href="{{ asset('1.jpg') }}" type="image/x-icon" />
     <!-- ===============================================-->
     <!--    Document Title-->
